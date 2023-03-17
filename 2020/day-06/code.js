36eebf4dfdd98e68ba12cb8c151a7028afa0e4e5
Hagay Haut <hagayhaut@gmail.com>
2023-03-16 20:57:44 -0400

    make shorterr

--- 2020/day-06/code.js
+++ 2020/day-06/code.js
@@ -4,9 +4,9 @@ const { join } = require("path");
 const input = fs.readFileSync(join(__dirname, "input.txt"), "utf-8");
 const sum = (a, b) => a + b;
 
-const processInput = (group) => {
+const processInput = (part) => {
     const groups = input.split('\n\n');
-    return groups.map(group === 1 ? processPart1Group : processPart2Group);
+    return groups.map(part === 1 ? processPart1Group : processPart2Group);
 }
 
 // number of unique responses per group
@@ -22,17 +22,10 @@ const processPart2Group = (group) => {
         .length;
 }
 
-const part1 = () => {
-    console.log(processInput(1).reduce(sum));
-}
+// part 1
+console.log(processInput(1).reduce(sum));
+// -> 6748
 
-// part1: 6748
-
-const part2 = () => {
-    console.log(processInput(2).reduce(sum));
-}
-
-// part2: 3445
-
-part1();
-part2();
+// part 2
+console.log(processInput(2).reduce(sum));
+// -> 3445
