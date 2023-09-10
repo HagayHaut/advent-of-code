2feed779bde91dd2f5cefd4e35aa99e85083732d
Hagay Haut <hagayhaut@gmail.com>
2023-09-09 21:28:30 -0400

    small optimization

--- 2019/day-02/script.js
+++ 2019/day-02/script.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const { join } = require('path');
 
-const input = fs.readFileSync(join(__dirname, 'input.txt'), 'utf-8');
+const program = fs.readFileSync(join(__dirname, 'input.txt'), 'utf-8')
+    .split(',')
+    .map(Number);
 
 function runProgram(program, noun, verb) {
     program[1] = noun;
@@ -17,7 +19,7 @@ function runProgram(program, noun, verb) {
     }
     return program[0];
 }
-const part1 = runProgram(input.split(',').map(Number), 12, 2);
+const part1 = runProgram([...program], 12, 2);
 console.log(part1);
 // part1: 5110675
 
@@ -25,7 +27,7 @@ let part2;
 const TARGET_AT_ADDRESS_0 = 19690720;
 for (let noun = 0; noun <= 99; noun++) {
     for (let verb = 0; verb <= 99; verb++) {
-        if (runProgram(input.split(',').map(Number), noun, verb) === TARGET_AT_ADDRESS_0) {
+        if (runProgram([...program], noun, verb) === TARGET_AT_ADDRESS_0) {
             part2 = 100 * noun + verb;
             break;
         }
