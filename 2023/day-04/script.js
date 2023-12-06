ace052f29b01c24aa551e4fd5e54ca70dbaa8fdb
Hagay Haut <hagayhaut@gmail.com>
2023-12-05 19:52:20 -0500

    day 4, part 2

--- 2023/day-04/script.js
+++ 2023/day-04/script.js
@@ -19,10 +19,8 @@ function processCard(line) {
     return { cardNumber, winningNumbers, guessedNumbers };
 }
 
-function getCardWinnings(card) {
-    let winningNumberCount = card.guessedNumbers
-        .filter(n => card.winningNumbers.has(n))
-        .length;
+function getCardWinningsPart1(card) {
+    let winningNumberCount = getWinningNumberCount(card);
     if (!winningNumberCount) return 0;
     let winnings = 1;
     while (winningNumberCount-- > 1) {
@@ -31,11 +29,38 @@ function getCardWinnings(card) {
     return winnings;
 }
 
+function getWinningNumberCount(card) {
+    return card.guessedNumbers
+        .filter(n => card.winningNumbers.has(n))
+        .length;
+}
+
 function part1() {
     return cards
-        .map(getCardWinnings)
+        .map(getCardWinningsPart1)
         .reduce((a, b) => a + b, 0);
 }
 
 // console.log(part1());
 // 18619
+
+function part2() {
+    const winCountPerCard = cards.map(getWinningNumberCount);
+    const map = {};
+
+    winCountPerCard.forEach((winCount, i) => {
+        map[i] ??= 0;
+        map[i]++;
+        for (let j = 0; j < winCount; j++) {
+            map[i + 1 + j] ??= 0;
+            map[i + 1 + j] += (map[i] ?? 0);
+        }
+    });
+
+    return Object
+        .values(map)
+        .reduce((a, b) => a + b, 0);
+}
+
+console.log(part2());
+// 8063216
