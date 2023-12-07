cc328d17eb1e6349fc4e6d76ed7873f57e9c6a83
Hagay Haut <hagayhaut@gmail.com>
2023-12-06 21:42:21 -0500

    day 6, part 1

---
+++ 2023/day-06/script.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const { join } = require("path");
+
+const input = fs.readFileSync(join(__dirname, "input.txt"), "utf-8");
+const [timeInput, distanceInput] = input.split('\n');
+const raceTimes = [...timeInput.match(/\d+/g)].map(Number);
+const raceDistances = [...distanceInput.match(/\d+/g)].map(Number);
+
+function calculateDistances(raceTime) {
+    const distances = [];
+    for (let speed = 0; speed < raceTime; speed++) {
+        const millisecondsLeft = raceTime - speed;
+        distances.push(millisecondsLeft * speed);
+    }
+    return distances;
+}
+
+function part1() {
+    const numberOfWaysToWin = raceTimes.map((raceTime, i) => {
+        const raceDistance = raceDistances[i];
+        const possibleDistances = calculateDistances(raceTime);
+        return possibleDistances.filter(d => d > raceDistance).length;
+    });
+
+    return numberOfWaysToWin.reduce((a, b) => a * b, 1);
+}
+
+console.log(part1());
+// 1731600
